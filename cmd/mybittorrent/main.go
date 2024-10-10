01bcbb0654d79f12fa3a65edd883302afe39f578
afif <izzuddinafif@gmail.com>
2024-10-10 15:12:01 +0700

    codecrafters submit [skip ci]

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 	"unicode"
 	// bencode "github.com/jackpal/bencode-go" // Available if you need it!
 )
@@ -34,8 +35,22 @@ func decodeBencode(bencodedString string) (interface{}, error) {
 		}
 
 		return bencodedString[firstColonIndex+1 : firstColonIndex+1+length], nil
+	} else if strings.HasPrefix(bencodedString, "i") {
+		eIndex := strings.Index(bencodedString, "e")
+		strNum := bencodedString[1:eIndex]
+		if strNum == "-0" {
+			return "", fmt.Errorf("-0 is not allowed")
+		}
+		if strings.HasPrefix(strNum, "0") && len(strNum) > 1 {
+			return "", fmt.Errorf("leading 0 is not allowed")
+		}
+		num, err := strconv.Atoi(strNum)
+		if err != nil {
+			return "", err
+		}
+		return num, nil
 	} else {
-		return "", fmt.Errorf("Only strings are supported at the moment")
+		return "", fmt.Errorf("not supported")
 	}
 }
 
@@ -46,16 +61,15 @@ func main() {
 	command := os.Args[1]
 
 	if command == "decode" {
-		// Uncomment this block to pass the first stage
-		
+
 		bencodedValue := os.Args[2]
-		
+
 		decoded, err := decodeBencode(bencodedValue)
 		if err != nil {
-		 	fmt.Println(err)
-		 	return
+			fmt.Println(err)
+			return
 		}
-		
+
 		jsonOutput, _ := json.Marshal(decoded)
 		fmt.Println(string(jsonOutput))
 	} else {
